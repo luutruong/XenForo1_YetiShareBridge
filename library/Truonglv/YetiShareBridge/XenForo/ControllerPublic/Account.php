160a848d95ac530b3875850e9b62068624c4b375
Luu Truong <truonglv@outlook.com>
2019-10-29 11:53:42 +0700

    Fixed dupe existing.

--- library/Truonglv/YetiShareBridge/XenForo/ControllerPublic/Account.php
+++ library/Truonglv/YetiShareBridge/XenForo/ControllerPublic/Account.php
@@ -27,8 +27,21 @@ class Truonglv_YetiShareBridge_XenForo_ControllerPublic_Account extends XFCP_Tru
         if ($accountInfo === null) {
             return $this->responseError(new XenForo_Phrase('yetishare_bridge_error_invalid_api_key'));
         }
+        if ($accountInfo['status'] === 'disabled') {
+            return $this->responseError(new XenForo_Phrase('yetishare_bridge_error_account_disabled'));
+        }
+
         /** @var XenForo_Model_UserExternal $userExternalModel */
         $userExternalModel = $this->getModelFromCache('XenForo_Model_UserExternal');
+
+        $existing = $userExternalModel->getExternalAuthAssociation(
+            Truonglv_YetiShareBridge_Helper_YetiShare::PROVIDER_EXTERNAL_USER,
+            $accountInfo['account_id']
+        );
+        if ($existing && $existing['user_id'] != XenForo_Visitor::getUserId()) {
+            return $this->responseError(new XenForo_Phrase('yetishare_bridge_error_account_integrated_with_other'));
+        }
+
         $userExternalModel->updateExternalAuthAssociation(
             Truonglv_YetiShareBridge_Helper_YetiShare::PROVIDER_EXTERNAL_USER,
             $accountInfo['id'],
