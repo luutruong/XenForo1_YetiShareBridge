db17700e503cb89a5e3a0ec3159c19d080d54f0d
Luu Truong <truonglv@outlook.com>
2019-10-29 10:11:44 +0700

    Update FileSums.php

--- library/Truonglv/YetiShareBridge/FileSums.php
+++ library/Truonglv/YetiShareBridge/FileSums.php
@@ -7,14 +7,15 @@ class Truonglv_YetiShareBridge_FileSums
         return array(
             'library/Truonglv/YetiShareBridge/Deferred/Downgrade.php' => 'b24a481480b1806013788735719f2228',
             'library/Truonglv/YetiShareBridge/Helper/Encrypt.php' => 'e0d5a3e80d19c7e7dc61d812a8ba6384',
-            'library/Truonglv/YetiShareBridge/Helper/Yetishare.php' => 'aa0ad1c67fd2f975c03140285f7d937a',
+            'library/Truonglv/YetiShareBridge/Helper/Yetishare.php' => '0ded8f9bb1bca53be89892e10367b6c4',
             'library/Truonglv/YetiShareBridge/Installer.php' => '0d474bd11b928d183aac9a52a2d4a9e8',
-            'library/Truonglv/YetiShareBridge/Listener.php' => '284dd96ac81a134b42aa9ce2a87dbb44',
+            'library/Truonglv/YetiShareBridge/Listener.php' => 'b0f79b737d1bde0b2e98a775440e0349',
             'library/Truonglv/YetiShareBridge/Model/Log.php' => '53ba8c5724dd394e3887da71687b1c8b',
             'library/Truonglv/YetiShareBridge/Option.php' => 'd920bff48bc1108b02a6dedf6bb53ebe',
             'library/Truonglv/YetiShareBridge/XenForo/ControllerPublic/Account.php' => '63b9693fc08d3bf3a33166ea180ef5d8',
-            'library/Truonglv/YetiShareBridge/XenForo/ControllerPublic/Login.php' => '9f12d9aaf07ccefd3a92aaaf92374e02',
-            'library/Truonglv/YetiShareBridge/XenForo/DataWriter/User.php' => '3d3e1b712a2bf282597f26c880c70a57',
+            'library/Truonglv/YetiShareBridge/XenForo/ControllerPublic/Login.php' => '81c3263f4bbe708378c6908e22b8ea9e',
+            'library/Truonglv/YetiShareBridge/XenForo/ControllerPublic/Logout.php' => '6e1c2e8d736c90eb0ad8621f8c3b3581',
+            'library/Truonglv/YetiShareBridge/XenForo/DataWriter/User.php' => 'c43640d13476815ca09adc8baecbdf60',
             'library/Truonglv/YetiShareBridge/XenForo/Model/UserUpgrade.php' => '883a4bb424c384dff476691c28ce062d',
         );
     }
